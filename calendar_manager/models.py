56c6d8d9107362ba2ab319f088e132309e1a3ada
Fah Divertise <fah@divertise.asia>
2019-06-28 14:27:19 +0700

    Initial project with models and settings

---
+++ calendar_manager/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+
+class Holiday(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    is_vacation = models.BooleanField(default=True)
+
+
+class LeaveRequest(models.Model):
+    PERSONAL = 0
+    SICK = 1
+    LEAVE_TYPE = (
+        (PERSONAL, "Personal"),
+        (SICK, "Sick"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.IntegerField('Leave Type', choices=LEAVE_TYPE, default=PERSONAL)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    reason = models.CharField(max_length=255)
+    is_approved = models.BooleanField(default=False)
+
+
+@receiver(pre_save, sender=LeaveRequest)
+def auto_approve_on_sick_type(sender, instance, **kwargs):
+    if instance.type == LeaveRequest.SICK:
+        instance.is_approved = True
