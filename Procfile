8b2b47f6d82d8acef37f5a928538e6b382579dce
Fah Divertise <fah@divertise.asia>
2019-09-19 21:30:44 +0700

    Change release step in Procfile to use start script

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate
+release: ./start.sh
 web: gunicorn config.wsgi --log-file -
--- start.sh
+++ start.sh
@@ -4,5 +4,3 @@ echo "RememberME start!"
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py init_super_user
-gunicorn --bind :8001 server.wsgi:application --daemon --workers 3 --timeout 3600
-nginx -g "daemon off;"
