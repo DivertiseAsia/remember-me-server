760f53b411c70bd5fae76c124ed90d5390ac2d29
Hunter Williams <hgw2es@virginia.edu>
2023-12-06 19:57:19 +0700

    added docker compose so it can run locally more easily

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,30 +1,30 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='PLEASE_SET_DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default="PLEASE_SET_DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ORIGIN_PROTOCOL = 'http'
-ORIGIN_URL = 'localhost'
+ORIGIN_PROTOCOL = "http"
+ORIGIN_URL = "localhost"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rememberme',
-        'USER': 'postgres',
-        'PASSWORD': '1234567q',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
-SUPER_PASSWORD = 'RememberME1234'
+SUPER_PASSWORD = "RememberME1234"
 
-SENDGRID_API_KEY = 'SG.PLEASE_ADD_SENDGRID_API_KEY'
-DEV_EMAIL = 'dev@divertise.asia'
-ADMIN_EMAIL = 'fah@divertise.asia'
+SENDGRID_API_KEY = "SG.PLEASE_ADD_SENDGRID_API_KEY"
+DEV_EMAIL = "dev@divertise.asia"
+ADMIN_EMAIL = "fah@divertise.asia"
--- start.sh
+++ start.sh
@@ -4,3 +4,4 @@ echo "RememberME start!"
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py init_super_user
+python manage.py runserver 0.0.0.0:8000
